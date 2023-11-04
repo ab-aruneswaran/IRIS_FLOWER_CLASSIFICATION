3aa4448c342b3c83cc01c8a3689a418648bd1325
arun1968333 <aruneswaranab@gmail.com>
2023-11-04 21:35:45 +0530

    Updation on ReadmeFile

--- iris_webapp.py
+++ iris_webapp.py
@@ -4,7 +4,6 @@ import pandas as pd
 
 lin_model=pickle.load(open('lin_model.pkl','rb'))
 log_model=pickle.load(open('log_model.pkl','rb'))
-svm=pickle.load(open('svm_model.pkl','rb'))
 
 def classify(num):
     if num<0.5:
@@ -36,10 +35,8 @@ def main():
     if st.button('Classify'):
         if ml_name=='Linear Regression':
             st.success(classify(lin_model.predict(inputs)))
-        elif ml_name=='Logistic Regression':
-            st.success(classify(log_model.predict(inputs)))
         else:
-           st.success(classify(svm.predict(inputs)))
+           st.success(classify(log_model.predict(inputs)))
 
 
 if __name__=='__main__':
