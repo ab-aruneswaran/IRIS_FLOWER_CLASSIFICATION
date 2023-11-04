ecc9fbc7e99b789a0f04c09ded9e64f8cf797dc4
arun1968333 <aruneswaranab@gmail.com>
2023-11-04 13:02:30 +0530

    update-before-deploy-ml-models

---
+++ README.md
@@ -0,0 +1,30 @@
+# IRIS-Flower-classification
+
+<img src="https://miro.medium.com/max/875/1*7bnLKsChXq94QjtAiRn40w.png">
+
+This Project is thorugh application of machine learning with python programming.
+It focuses on IRIS flower classification using Machine Learning with scikit tools. 
+Here some of algorithm are used that are some types of machine learning subparts algorithms of supervised and Unsupervised learning.
+Algorithm used for predicting and get accuracy are -
+1. SVm
+2. Logistic Regression
+3. K nearest classifier
+4. Decision tree classifier
+5. metrics
+6. train_test_split
+We are making accuracy and prediction in Iris project through Iris Dataset.
+This is easy and understable for machine learning staters( Naive ).
+
+#### -- Project Status: [Completed]
+
+### Getting Started
+1. You can access the raw data [here](https://github.com/ab-aruneswaran/Iris_Flower_Classification/blob/main/dataset/iris.csv) within this repo.
+2. All of the scripts are being kept [here](https://github.com/ab-aruneswaran/Iris_Flower_Classification/blob/main/iris_model.ipynb).
+
+
+### Result 
+
+ This Machine Learning Model achieves a high accuracy of 100% using all of above Algorithms .
+#### I hope this will be useful someday, thankyou for seeing !✌🏻
+
+PS: Please do not forget to drop a star if you like it!
---
+++ iris_webapp.py
@@ -0,0 +1,46 @@
+import streamlit as st
+import pickle
+import pandas as pd
+
+lin_model=pickle.load(open('Models\lin_model.pkl','rb'))
+log_model=pickle.load(open('Models\log_model.pkl','rb'))
+svm=pickle.load(open('Models\svm_model.pkl','rb'))
+
+def classify(num):
+    if num<0.5:
+        st.image('Images\Iris_Setosa.jpg')
+        return 'Iris-Setosa'
+    elif num <1.5:
+        st.image('Images\Iris_Versicolor.jpg')
+        return 'Iris-Versicolor'
+    else:
+        st.image('Images\Iris_Virginica.jpg')
+        return 'Iris-Virginica'
+def main():
+    st.title("WELCOME TO CLASSIFICATION WORLD")
+    html_temp = """
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">IRIS FLOWER CLASSIFICATION</h2>
+    </div>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+    sl=float(st.number_input('Enter Sepal Length'))
+    sw=float(st.number_input('Enter Sepal Width'))
+    pl=float(st.number_input('Enter Petal Length'))
+    pw=float(st.number_input('Enter Petal Width'))
+    inputs=[[sl,sw,pl,pw]]
+    options = pd.DataFrame({
+    'Model Name': ["Linear Regression", "Logistic Regression", "SVM"],
+    })
+    ml_name = st.selectbox("Choose Your ML Model", options)
+    if st.button('Classify'):
+        if ml_name=='Linear Regression':
+            st.success(classify(lin_model.predict(inputs)))
+        elif ml_name=='Logistic Regression':
+            st.success(classify(log_model.predict(inputs)))
+        else:
+           st.success(classify(svm.predict(inputs)))
+
+
+if __name__=='__main__':
+    main()
